461ee7547ccce365f5ad67c2b01621d0db16c137
= <=>
2019-11-28 22:06:20 +0800

    cc

---
+++ ConsoleForAzDevOps/ConsoleForAzDevOps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleForAzDevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
